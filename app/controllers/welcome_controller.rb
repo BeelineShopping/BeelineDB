9d6a63ef394c7d10c5bf88e32a7b70a5d69ed043
Vilyan01 <bheller2010@live.com>
2016-01-22 13:45:42 -0800

    Dummy endpoint

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def alive
+  	render :json => {:store => {:id => 1, :name => "Safeway", :aisle => {:id => 1, :section => {:number => 1, :items => [{:id => 1, :name => "Chicken"},{:id => 2, :name => "Brocolli"}]}}}}, :status => 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :stores
   root 'welcome#index'
+  get 'alive', :to => 'welcome#alive'
 end
