9c4e8ba0fdebdcef2498235592909d28a215278e
Vilyan01 <bheller2010@live.com>
2016-02-16 14:09:53 -0800

    Fixed problem with devise

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,6 @@ module Api
 	module V1
 		class ApiController < ApplicationController
 			skip_before_filter :verify_authenticity_token
-			#before_filter :check_auth
 
 			def check_authentication
 				resource = AppUser.find_by_email(username)
@@ -11,14 +10,6 @@ module Api
 				end
 			end
 
-  			#def check_auth
-    		#	authenticate_or_request_with_http_basic do |username,password|
-      		#		resource = AppUser.find_by_email(username)
-      		#		if resource.valid_password?(password)
-        	#			sign_in :user, resource
-      		#		end
-    		#	end
-    		#end
 			rescue_from 'Apipie::ParamMissing' do |exception|
 			  render :json => {:param_missing => "Missing parameter"}, :status => 422
 			end
