85e8778a4c07a3c810753595082762e309c61817
Vilyan01 <bheller2010@live.com>
2016-02-16 14:07:35 -0800

    Updated apipie

--- app/controllers/api/v1/store_controller.rb
+++ app/controllers/api/v1/store_controller.rb
@@ -1,16 +1,10 @@
 module Api
   module V1
     class StoreController < ApiController
-      api :GET, '/stores'
-      description "Returns a list of stores"
-      formats ['json']
       def list_stores
       	# if no params, return all stores
         render :json => {:stores => Store.all}
       end
-
-      api :GET, '/stores/:store_id'
-      description "Renders a list of items located in the store"
       def store_items
         store = Store.find(params[:store_id])
         render :json => store, :include => {:aisles =>  {:only => :aisle_number, :include => {:sections => {:only => :section_number, :include => {:items => {:only => [:id, :title]}}}}}}, :status => 200
--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -2,6 +2,9 @@ module Api
   module V1
     class UserController < ApiController
       before_action :authenticate_app_user!
+
+      api :GET, '/user'
+      description "Checks to see if the current user is logged in"
       def logged_in
         render :json => current_app_user
       end
