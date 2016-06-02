7cdc9a8129eb1ea13ee2ee1a882b551cb7bc3406
Vilyan01 <bheller2010@live.com>
2016-06-01 20:03:30 -0700

    Added form to add item to list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,5 +2,10 @@ class RecipesController < ApplicationController
 	def index
 		@search_term = params[:looking_for] || 'chicken'
 		@recipes = Recipe.for(@search_term)
+		if current_app_user
+			@lists = current_app_user.shopping_lists
+		else
+			@lists = []
+		end
 	end
 end
